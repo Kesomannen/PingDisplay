6e0684fac199ded708801cfc709edf6959f45fc7
Kesomannen <bobbo.nidor@gmail.com>
2024-02-24 23:34:24 +0100

    Added DisplayPosition config, 1.1.0

--- PingDisplay/Plugin.cs
+++ PingDisplay/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using TMPro;
 using Unity.Netcode;
@@ -11,24 +12,77 @@ namespace PingDisplay;
 public class Plugin : BaseUnityPlugin {
     static TMP_Text _pingText;
     static float _lastUpdateTime;
-    static bool _showPing;
+    
+    static ConfigEntry<bool> _showPingConfig;
+    static ConfigEntry<DisplayPosition> _displayPositionConfig;
 
     const float UpdateInterval = 0.5f;
+    const float Margin = 0.03f;
     
     void Awake() {
-        var showPingConfig = Config.Bind(
+        SetupConfig(_showPingConfig = Config.Bind(
             "General",
             "ShowPing",
             true,
             "Show your latency to the host in the top left corner of the screen"
-        );
+        ), value => _pingText.enabled = value);
         
-        _showPing = showPingConfig.Value;
-        showPingConfig.SettingChanged += (_, _) => _showPing = showPingConfig.Value;
+        SetupConfig(_displayPositionConfig = Config.Bind(
+            "General",
+            "DisplayPosition",
+            DisplayPosition.TopLeft,
+            "Where on the HUD to display your latency"
+        ), PositionDisplay);
         
         Harmony.CreateAndPatchAll(typeof(Plugin));
         
         Logger.LogInfo("Plugin loaded!");
+    }
+    
+    enum DisplayPosition {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight
+    }
+    
+    static void PositionDisplay(DisplayPosition position) {
+        if (_pingText == null) return;
+        
+        var rect = _pingText.rectTransform;
+        var pivot = position switch {
+            DisplayPosition.TopLeft => new Vector2(0f, 1f),
+            DisplayPosition.TopRight => new Vector2(1f, 1f),
+            DisplayPosition.BottomLeft => new Vector2(0f, 0f),
+            DisplayPosition.BottomRight => new Vector2(1f, 0f),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        
+        rect.pivot = pivot;
+        AddMargin(ref pivot.x);
+        AddMargin(ref pivot.y);
+        
+        rect.anchorMin = rect.anchorMax = pivot;
+        rect.anchoredPosition = Vector2.zero;
+        
+        _pingText.alignment = position switch {
+            DisplayPosition.TopLeft => TextAlignmentOptions.TopLeft,
+            DisplayPosition.TopRight => TextAlignmentOptions.TopRight,
+            DisplayPosition.BottomLeft => TextAlignmentOptions.BottomLeft,
+            DisplayPosition.BottomRight => TextAlignmentOptions.BottomRight,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        
+        return;
+
+        void AddMargin(ref float value) {
+            if (value == 0) value += Margin;
+            else value -= Margin;
+        }
+    }
+
+    static void SetupConfig<T>(ConfigEntry<T> config, Action<T> changedHandler) {
+        config.SettingChanged += (_, _) => changedHandler(config.Value);
     }
 
     [HarmonyPostfix]
@@ -40,14 +94,10 @@ public class Plugin : BaseUnityPlugin {
         _pingText = textGameObject.AddComponent<TextMeshProUGUI>();
         _pingText.faceColor = new Color32(255, 255, 255, 40);
         _pingText.font = __instance.weightCounter.font;
-        _pingText.alignment = TextAlignmentOptions.TopLeft;
         _pingText.fontSize = 12;
         
-        var rect = _pingText.rectTransform;
-
-        rect.pivot = new Vector2(0f, 1f);
-        rect.anchorMin = rect.anchorMax = new Vector2(0.03f, 0.97f);
-        rect.anchoredPosition = new Vector2(0f, 0f);
+        PositionDisplay(_displayPositionConfig.Value);
+        _pingText.enabled = _showPingConfig.Value;
         
         var canvasGroup = textGameObject.AddComponent<CanvasGroup>();
         canvasGroup.alpha = 1f;
@@ -65,15 +115,10 @@ public class Plugin : BaseUnityPlugin {
     [HarmonyPostfix]
     [HarmonyPatch(typeof(HUDManager), "Update")]
     static void HUDManager_Update_Postfix(HUDManager __instance) {
-        if (_pingText == null) return;
-
+        if (_pingText == null || !_pingText.enabled) return; 
+        
         if (Time.time - _lastUpdateTime < UpdateInterval) return;
         _lastUpdateTime = Time.time;
-        
-        if (!_showPing) {
-            _pingText.text = "";
-            return;
-        }
 
         if (NetworkManager.Singleton.IsHost) {
             _pingText.text = "Ping: ---";
